503a150364d0e0e96d5f3ed39f6e2e727c8ec8b6
sguha-work <sguha1988.life@gmail.com>
2018-03-02 08:59:07 +0530

    edit profile work continues

--- public/app/controllers/user/editprofileCtrl.js
+++ public/app/controllers/user/editprofileCtrl.js
@@ -1,10 +1,10 @@
 app.controller('editProfileCtrl', ['$scope', 'CONSTANT', '$http',
     '$routeParams', 'Facebook', 'socket', 'authService',
-    '$location', '$timeout', 'toastr', '$base64','profileService','$rootScope',
+    '$location', '$timeout', 'toastr', '$base64', 'profileService', '$rootScope',
     function ($scope, CONSTANT, $http, $routeParams, Facebook,
         socket, authService, $location, $timeout,
         toastr, $base64, profileService, $rootScope) {
-        
+
 
         $scope.rating = {
             current: 1,
@@ -14,7 +14,7 @@ app.controller('editProfileCtrl', ['$scope', 'CONSTANT', '$http',
         $scope.profile = {};
         $scope.draft = {};
         $scope.showFirst = true;
-        
+
         $scope.getUserProfileById = function (id) {
             profileService.getUserById(id)
                 .then(function (response) {
@@ -26,7 +26,7 @@ app.controller('editProfileCtrl', ['$scope', 'CONSTANT', '$http',
                     $scope.getUserDrafts(response.data[0]._id);
                     splitdate(new Date($scope.profile.dateOfBirth));
                 })
-                .catch(function(error){
+                .catch(function (error) {
                     toastr.error(error);
                 });
 
@@ -35,11 +35,11 @@ app.controller('editProfileCtrl', ['$scope', 'CONSTANT', '$http',
         $scope.getUserRating = function (id) {
             profileService.getUserRating(id)
                 .then(function (response) {
-                  console.log(response);
-                  $scope.rating.current = response.data.count;
-                  $("#rating_div").addClass("disabledbutton");
+                    console.log(response);
+                    $scope.rating.current = response.data.count;
+                    $("#rating_div").addClass("disabledbutton");
                 })
-                .catch(function(error){
+                .catch(function (error) {
                     toastr.error(error);
                 });
         }
@@ -51,30 +51,30 @@ app.controller('editProfileCtrl', ['$scope', 'CONSTANT', '$http',
                     $scope.profile.drafts = response.data.post;
                     $scope.getFavourites();
                 })
-                .catch(function(error){
+                .catch(function (error) {
                     toastr.error(error);
                 });
         }
-        
-        $scope.getFavourites = function(){
+
+        $scope.getFavourites = function () {
             $http({
                 url: CONSTANT.API_BASE_URL + 'getfavourites',
                 method: "GET",
             }).then(function (response) {
-                if(response.data.success){
-                  console.log(response);
-                    $scope.favourites = response.data.data; 
-                }else{
-                      console.log("err");
-                    }
-                },
+                if (response.data.success) {
+                    console.log(response);
+                    $scope.favourites = response.data.data;
+                } else {
+                    console.log("err");
+                }
+            },
                 function (response) { // optional
                     console.log("some error occured");
                 });
-          
-              }
 
-        $scope.editPost = function(draft){
+        }
+
+        $scope.editPost = function (draft) {
             $scope.showedit = false;
             $scope.draft.postTopic = draft.postTopic;
             $scope.draft.postTitle = draft.postTitle;
@@ -84,66 +84,73 @@ app.controller('editProfileCtrl', ['$scope', 'CONSTANT', '$http',
             $scope.draft.id = draft._id;
         }
 
-        $scope.updateDraft = function(post){
+        $scope.updateDraft = function (post) {
             post.userId = $scope.profile._id;
-            post.poster = $scope.profile.firstName +" "+ $scope.profile.lastName;
+            post.poster = $scope.profile.firstName + " " + $scope.profile.lastName;
             post.posterImage = $scope.profile.profilePic;
-          
+
             $http({
                 method: 'POST',
-                url: CONSTANT.API_BASE_URL+'updatedraft',
+                url: CONSTANT.API_BASE_URL + 'updatedraft',
                 data: post,
                 headers: {
                     'Content-type': 'application/json;charset=utf-8'
                 }
             })
-            .then(function(response) {
-                $scope.getUserDrafts();
-                console.log(response.data);
-            }, function(rejection) {
-                console.log(rejection.data);
-            });
-          }
-        
-        
-        $scope.removePost = function(draft){
+                .then(function (response) {
+                    $scope.getUserDrafts();
+                    console.log(response.data);
+                }, function (rejection) {
+                    console.log(rejection.data);
+                });
+        }
+
+
+        $scope.removePost = function (draft) {
             $http({
                 method: 'DELETE',
-                url: CONSTANT.API_BASE_URL+'deletepost',
+                url: CONSTANT.API_BASE_URL + 'deletepost',
                 data: { id: draft.id },
                 headers: {
                     'Content-type': 'application/json;charset=utf-8'
                 }
             })
-            .then(function(response) {
-                $scope.getUserDrafts();
-                console.log(response.data);
-            }, function(rejection) {
-                console.log(rejection.data);
-            });
+                .then(function (response) {
+                    $scope.getUserDrafts();
+                    console.log(response.data);
+                }, function (rejection) {
+                    console.log(rejection.data);
+                });
         }
 
-        $scope.countOf = function(text) {
+        $scope.countOf = function (text) {
             var s = text ? text.split(/\s+/) : 0; // it splits the text on space/tab/enter
-            return s ? s.length-1 : 0;
+            return s ? s.length - 1 : 0;
         };
-        
-        $scope.goNext = function(){
-                $scope.showFirst = false;
-             }
-       
-        $scope.goPrevious = function(){
-               $scope.showFirst = true;
+
+        $scope.goNext = function () {
+            $scope.showFirst = false;
+        }
+
+        $scope.goPrevious = function () {
+            $scope.showFirst = true;
+        }
+
+        $scope.closeProfile = function () {
+            $location.path("/home");
+        }
+
+        $scope.editProfile = function () {
+            if ($scope.editMode) {
+                $scope.editMode = false;
+                toastr.success("Edit mode is off");
+            } else {
+                $scope.editMode = true;
+                toastr.success("Edit mode is on");
             }
 
-        $scope.closeProfile = function(){
-            $location.path("/page");
         }
-
-        $scope.editProfile = function(){
-            $scope.editMode = true;
-        }
-        $scope.closeDialog = function(){
+        $scope.closeDialog = function () {
             $scope.draft = {};
             $scope.showFirst = true;
         }
@@ -152,34 +159,34 @@ app.controller('editProfileCtrl', ['$scope', 'CONSTANT', '$http',
             $scope.getUserProfileById($routeParams.id);
             $scope.getUserRating($routeParams.id);
 
-        }else{
+        } else {
             $scope.getUserProfileById($rootScope.current_user._id);
             $scope.getUserRating($rootScope.current_user._id);
         }
 
-        $scope.openPrevModel = function(){
+        $scope.openPrevModel = function () {
             angular.element('#modalsharepost').modal('hide');
-           
+
             angular.element('#modalshare').modal('show');
-            
-            
-          }
-      
+
+
+        }
+
         //   $scope.options = {
         //     language: 'en',
         //     allowedContent: true,
         //     entities: false,
         //     removePlugins :'elementspath,save,font'
         //   };
-        
 
-        var splitdate = function(dateObj){
+
+        var splitdate = function (dateObj) {
             $scope.profile.month = dateObj.getUTCMonth() + 1; //months from 1-12
             $scope.profile.date = dateObj.getUTCDate();
             $scope.profile.year = dateObj.getUTCFullYear();
         }
 
-        var getRelativePath = function(path){
+        var getRelativePath = function (path) {
             var index = path.indexOf('app');
             return path.substring(index);
         }
