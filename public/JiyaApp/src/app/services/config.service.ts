495966d6a962c385dfc68a6b25aed6f851261e91
sguha-work <sguha1988.life@gmail.com>
2018-05-14 13:11:44 +0530

    login sign up work continues

---
+++ public/JiyaApp/src/app/services/config.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+
+  constructor() { 
+
+  }
+
+  /**
+   * This method returns the api base url by checking the environment
+   */
+  public getBaseURL(): string {
+    let location: string = window.location.href;
+    let apiBaseURL: string = "";
+    if (typeof window.localStorage.apiBaseUrl === "undefined" || window.localStorage.apiBaseUrl === "") {
+      if (location.indexOf("jiyatech.au-syd.mybluemix.net")) {
+        apiBaseURL = "https://jiyatech.au-syd.mybluemix.net/api/";
+      } else {
+        apiBaseURL = "http://192.168.56.101:4200/api/";
+      }
+      window.localStorage.apiBaseUrl = apiBaseURL;
+    }
+    return window.localStorage.apiBaseUrl;
+  }
+}
--- public/JiyaApp/src/app/services/user.service.ts
+++ public/JiyaApp/src/app/services/user.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import {_LoginData} from './../interfaces/login-data';
+import { ConfigService } from './config.service';
+import { _LoginData } from './../interfaces/login-data';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private config: ConfigService) { }
 
   /**
    * This function checks for any active session
@@ -26,7 +27,7 @@ export class UserService {
 
   public login(loginData: _LoginData): Promise<any> {
     return new Promise((resolve, reject) => {
-      this.http.post('/api/authenticate', loginData).subscribe((data: any) => {
+      this.http.post(this.config.getBaseURL() + 'authenticate', loginData).subscribe((data: any) => {
         if (data.data.token) {
           this.setToken(data.data.token);
           resolve(data.data.token);
@@ -46,5 +47,37 @@ export class UserService {
     } else {
       return false;
     }
+  }
+
+  /**
+   * 
+   * @param email 
+   * @desc call api to send the password reset otp
+   */
+  public sendPasswordResetOTP(email: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.config.getBaseURL() + 'sendResetPasswordOTP', { "email": email }).subscribe((response) => {
+        resolve(response);
+      }, (error) => {
+        reject(error);
+      });
+    });
+  };
+
+  /**
+   * 
+   * @param email 
+   * @param otp 
+   * @param password
+   * @desc call the api to reset the password 
+   */
+  public resetPassword(email: string, otp: string, password: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.config.getBaseURL() + 'resetpassword', { "email": email, "otp": otp, "password": password }).subscribe((response) => {
+        resolve(response);
+      }, (error) => {
+        reject(error);
+      });
+    });
   }
 }
