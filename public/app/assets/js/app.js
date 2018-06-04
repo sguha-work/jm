13cffba661a8699f9e7e2c5565da3c5b090729e4
sguha-work <sguha1988.life@gmail.com>
2018-06-04 21:36:45 +0530

    new ui integrating

--- public/app/assets/js/app.js
+++ public/app/assets/js/app.js
@@ -3,4 +3,21 @@ jQuery(document).ready(function($) {
 		factor: '-0.5'
 	});
 	$("html").easeScroll();
+
+	$("#forgetPassword").animatedModal({
+		color:'rgba(156,25,25,0.8)',
+	});
+
+	$('.sendOtp').click(function(event) {
+		$('#stepOne').css('display', 'none');
+		$('#stepTwo').css('display', 'block');
+	});
+
+	$('.resetPass').click(function(event) {
+		$('.close-animatedModal').trigger('click');
+		setTimeout(function(){
+			$('#stepOne').css('display', 'block');
+			$('#stepTwo').css('display', 'none');
+		}, 600);
+	});
 });
